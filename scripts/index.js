839031e330ed5297e1c7271aa01cd349e020782a
Vic G14 <victor.a.paniagua.a@gmail.com>
2022-07-05 16:08:57 -0700

    Background image change and navbar in progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,17 @@
-window.onscroll = function() {myFunction()};
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
 
-var navigation = document.getElementById("navigation")
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
 
-var sticky = navigation.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-      navigation.classList.add("sticky");
-    } else {
-      navigation.classList.remove("sticky");
-    };
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
